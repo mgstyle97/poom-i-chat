6ce7d7791ef160a69786f96024532de199491abd
Mingi Kim <migni4575@gmail.com>
2021-10-28 15:26:49 +0900

    Initial commit

---
+++ src/main/java/io/wisoft/poomi/chat/PoomIChatApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.poomi.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoomIChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoomIChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wisoft/poomi/chat/PoomIChatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.poomi.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoomIChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
